b28971075744173e8e9c76dea583a9c133102ac9
Oscar Ballesteros <ext_orballesteros@falabella.cl>
2021-06-19 21:35:35 -0400

    add removeSName test + function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-.js
+const removeSName = (names) => {
+    // let expectedOutput = names;
+    // names.forEach(element => {
+    //     if(element.toLowerCase().charAt(0) === 's'){
+    //         const index = expectedOutput.indexOf(element);
+    //         expectedOutput.splice(index, 1);
+    //     }       
+    // });
+    // return expectedOutput;
+    return names.filter(element => element.toLowerCase().charAt(0) !== 's');
+};
+
+module.exports = removeSName;
--- app.test.js
+++ app.test.js
@@ -1,1 +1,25 @@
-es
+const removeSNames = require('./app');
+
+describe('removeSNames', () => {
+
+    // Test 1 - name with 'S' should not be present in the resultant array 
+    it('should remove all S names', () => {
+        const names = ['Apple', 'Strawberry', 'Star fruit'];
+        expect(removeSNames(names)).not.toContain('Strawberry');
+        expect(removeSNames(names)).not.toContain('Star fruit');
+    });
+
+    // Test 2 names without 'S' should remain untouched 
+    it('should not remove other names', () => {
+        const names = ['Apple', 'Strawberry', 'PineApple'];
+        expect(removeSNames(names)).toContain('Apple');
+        expect(removeSNames(names)).toContain('PineApple');
+    });
+
+    // Test 3 names with 'S' or 's' should not be present 
+    it('should mind the case', () => {
+        const names = ['Apple', 'Strawberry', 'strawberry', 'PineApple'];
+        expect(removeSNames(names)).not.toContain('Strawberry');
+        expect(removeSNames(names)).not.toContain('strawberry');
+    });
+})
